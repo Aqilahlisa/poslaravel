f96a926b654aecd46f1f60fe9a374a5866d0052d
Herry Prasetyo <herry081288@gmail.com>
2022-11-10 14:07:22 +0700

    indexcategory

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,3 +1,23 @@
 @extends('master')
 
+@section('title')
+    <title>Halaman Category</title>
+@endsection
 
+@section('content')
+    <section class="section">
+        <div class="section-header">
+            <h1>Halaman Category</h1>
+        </div>
+        <h2 class="section-title">Halaman Category</h2>
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <a href="" class="btn btn-primary">Tambah</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
